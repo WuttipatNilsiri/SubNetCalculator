a6908f281815bb53ddcc8911de993aeb88fd4d48
wuttipat.ni@ku.th <wuttipat.ni@ku.th>
2018-11-28 12:08:41 +0900

    catch error

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request
 import genlist
 import numpy as np
 import math
+import ipaddress
 app = Flask(__name__)
 
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
@@ -37,16 +38,29 @@ def cal():
         if IP == '':
             return render_template('app.pug', resultarray=[])
 
+        try:
+            ipaddress.ip_address(IP)
+        except ValueError:
+            return render_template('app.pug', error='IP error' , IP='160.10.0.0' , hostNum=10 , samNum=3)
+
+
         c , fix = genlist.defClass(IP)
 
         if hostNum:
-            netNum = 2**(32 - fix - math.ceil( math.log2(int(hostNum) ) ))
-    
-        s , a , info = genlist.run(str(IP),int(netNum),int(samNum))
+            try:
+                netNum = 2**(32 - fix - math.ceil( math.log2(int(hostNum) ) ))
+            except ValueError:
+                return render_template('app.pug', error='#value error' , IP='160.10.0.0' , hostNum=10 , samNum=3 , host='on')
+
+        try:
+            s , a , info = genlist.run(str(IP),int(netNum),int(samNum))
+        except ValueError:
+            return render_template('app.pug', error='#value error', IP='160.10.0.0' , hostNum=10 , samNum=3 )
+        
         if hostNum:
             return render_template('app.pug', resultarray=a , IP=IP , samNum=samNum , info=info , hostNum=hostNum , host='on')
         else:
             return render_template('app.pug', resultarray=a , IP=IP , netNum=netNum , samNum=samNum , info=info )
  
 if __name__ == "__main__":
-    app.run()
+    app.run(host='158.108.143.148')
